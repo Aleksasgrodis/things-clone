c3572694302f61bedc6cc3d7d3fd532ff495f812
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-09-08 19:54:09 +0200

    Implemented a selector library (Reselect) to prevent unnecessary re renders when using useSelector

--- src/components/ProjectView.jsx
+++ src/components/ProjectView.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { createSelector } from 'reselect';
+
+const selectProjectWithId = createSelector(
+  state => state.projects,
+  (_, projectID) => projectID,
+  (projects, projectID) => projects.find(({ id }) => id === projectID),
+);
+
+const selectTasksOfProject = createSelector(
+  state => state.tasks,
+  (_, projectID) => projectID,
+  (tasks, projectID) => tasks.filter(({ parent }) => parent === projectID),
+);
 
 function ProjectView(props) {
   const { projectID } = useParams();
-  const {
-    project: { title, headings, tags, notes },
-    tasks,
-  } = useSelector(state => ({
-    project: state.projects.find(({ id }) => id === projectID),
-    tasks: state.tasks.filter(({ parent }) => parent === projectID),
-  }), shallowEqual);
+  const projectTasks = useSelector(state =>
+    selectTasksOfProject(state, projectID),
+  );
+  const { title, headings, tags, notes } = useSelector(state =>
+    selectProjectWithId(state, projectID),
+  );
   return (
     <div>
-      {title}
+      <h2>{title}</h2>
+      <span>{notes}</span>
       {headings}
-      {tasks.map(({title, id}) => (<p key={id}>{title}</p>))}
+      {projectTasks.map(({ title, id }) => (
+        <p key={id}>{title}</p>
+      ))}
     </div>
   );
 }
