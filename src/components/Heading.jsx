4051e934bb25519128b9c5fb51e83f1e05097b1d
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-09-13 14:19:10 +0200

    Added the same unfocus-ability to headings withing ProjectView

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -11,21 +11,39 @@ function Heading(props) {
     drop: item => changeTaskHeader(item),
   });
   const dispatch = useDispatch();
+  const node = React.useRef();
   const changeTaskHeader = ({ id }) => {
     dispatch(editTodoHeading({ id, heading: props.id }));
   };
+
+  React.useEffect(() => {
+    const handleClick = e => {
+      if(!node.current.contains(e.target)){
+        props.setSelectedHeading(null)
+      }
+    }
+    document.addEventListener('mousedown', handleClick)
+    return () => {
+      document.removeEventListener('mousedown', handleClick)
+    }
+  }, [props])
+
   return (
-    <div className="heading-wrapper" ref={drop}>
-      <div
-        className={`heading ${
-          props.selectedHeading === props.id ? 'selected' : ''
-        }`}
-        onClick={() => props.setSelectedHeading(props.id)}
-      >
-        <p>{props.title}</p>
-      </div>
-      <div className="tasks">
-        {props.tasks.length ? props.tasks.map(t => <Task key={t.id} {...t} />) : null}
+    <div ref={node}>
+      <div className="heading-wrapper" ref={drop}>
+        <div
+          className={`heading ${
+            props.selectedHeading === props.id ? 'selected' : ''
+          }`}
+          onClick={() => props.setSelectedHeading(props.id)}
+        >
+          <p>{props.title}</p>
+        </div>
+        <div className="tasks">
+          {props.tasks.length
+            ? props.tasks.map(t => <Task key={t.id} {...t} />)
+            : null}
+        </div>
       </div>
     </div>
   );
